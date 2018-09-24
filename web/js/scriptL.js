97c5962313de22d491b2603878fb24bac4b09cb0
Andre Oliveira <Andre.Oliveira@la.logicalis.com>
2018-09-24 13:58:07 -0300

    Livro carregar

--- web/js/scriptL.js
+++ web/js/scriptL.js
@@ -3,7 +3,7 @@
   var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "http://digito.azurewebsites.net/livro",
+    "url": "http://digito.azurewebsites.net/livro/",
     "method": "GET",
     "headers": {
       "Cache-Control": "no-cache",
@@ -15,13 +15,43 @@
     console.log(response);
 
     var livrosisbn = "";
-
     $.each(response,function(i,response){
-      livrosisbn += "<option value='"+response.idlivro+"'>"+response.isbn+"</option>";
-    });
+     
 
-    $("#livros").append(livrosisbn);
+     $("#livros").append("<option value='"+response.idlivro+"'>"+response.isbn+"</option>");
+     buscaimg(response.isbn);
+     console.log(response.isbn);
+   });
+
 
   });
 
-});
+
+   // Buscar livro selecionado
+   function buscaimg(livros){
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://www.googleapis.com/books/v1/volumes?q="+livros,
+      "method": "GET",
+      "printType": "books",
+      "maxResults": "40",
+      "headers": {
+        "Cache-Control": "no-cache"
+      }
+    }
+
+    $.ajax(settings).done(function (response) { 
+      console.log(response);
+      $("#listalivros").append(
+      "<li class=book"+livros+"><a href=''> <img src="+response.items[0].volumeInfo.imageLinks.smallThumbnail+"alt='' /></a></li>");
+         //alert(response.items[0].volumeInfo.title);
+       }).fail(function (response) { 
+        console.log(response);
+      });
+
+
+
+     };
+
+   });
