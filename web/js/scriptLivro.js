572ba4685fa6bb74831e875590e965a4d24fa7de
Andre Oliveira <Andre.Oliveira@la.logicalis.com>
2018-09-28 09:15:07 -0300

    Estruturação dos scripts

--- web/js/scriptLivro.js
+++ web/js/scriptLivro.js
@@ -172,3 +172,16 @@ function buscaimgReservado(livros){
 
 };
 
+// Efeito Collapsible
+function expandAll(){
+  $(".collapsible-header").addClass("active");
+  $(".collapsible").collapsible({accordion: false});
+}
+
+function collapseAll(){
+  $(".collapsible-header").removeClass(function(){
+    return "active";
+  });
+  $(".collapsible").collapsible({accordion: true});
+  $(".collapsible").collapsible({accordion: false});
+}
