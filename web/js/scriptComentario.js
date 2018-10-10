4484086b986600d84e190387c5c2531eb9a4268e
Andre Oliveira <Andre.Oliveira@la.logicalis.com>
2018-10-10 09:33:33 -0300

    Criação do comentário

---
+++ web/js/scriptComentario.js
@@ -0,0 +1,37 @@
+$(function(){
+  var inDexValue;
+  
+    $('button').click( function() {
+      if($('#userCmnt').val().length == ''){
+       alert('Please Enter Your Comment');
+       return true;
+      }
+      var userCmnt = $('#userCmnt').val();
+      if($('#submit').hasClass('editNow')){
+                 
+        $('#cmntContr>div.viewCmnt').eq(inDexValue).children('p').html(userCmnt);
+        
+      }else{      
+    
+    $('#cmntContr').append("<div class='viewCmnt'><p>"+ userCmnt + "</p><span class='edit'></span><span class='delete'></span></div>");
+     }
+      $('#userCmnt').val('');
+      $(this).removeClass('editNow');
+  });
+    
+  // Delete 
+  $('#cmntContr').on('click', '.delete', function(){   
+    confirm("Excluir comentário");
+    $(this).parent().remove();
+  });
+  // Edit
+  $('#cmntContr').on('click', '.edit', function(){
+   
+    var toEdit = $(this).prev('p').html();
+    //alert(toEdit);
+    $('#userCmnt').val(toEdit);
+    $('button').addClass('editNow');
+    inDexValue = $(this).parent('div.viewCmnt').index();
+    
+  });
+  });
--- web/js/scriptForum.js
+++ web/js/scriptForum.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
 	carregarForum();
-
 });
 
 //======================================== metodos ==================================================
@@ -27,6 +26,7 @@ function carregarForum(){
 
 }
 
+
 // Buscar livro pelo isbn
 function buscaimg(livros){
 	var settings = {
@@ -46,14 +46,15 @@ function buscaimg(livros){
 		console.log(response.items[0].volumeInfo.authors);
 		console.log(response.items[0].volumeInfo.imageLinks.smallThumbnail);
 		$.ajax(settings).done(function (response) { 
-			$("#listaforuns").append("<div class='c-forum t-forum'><a class='c-forum-body t-forum-body' href=''>\
-				<span class='c-forum-media t-forum-media'><svg class='o-icon c-forum-media-icon-read t-forum-media-icon-read'><use xlink:href='#forum'></use></svg></span>\
+			$("#listaforuns").append("<div  id= 'caixaLivro'  class='c-forum t-forum'><a  id= 'caixa' class='t-forum-body'href=''>\
+				<span class='c-forum-media t-forum-media'><svg class='o-icon c-forum-media-icon-read t-forum-media-icon-read'>\
+				<use xlink:href='#forum'></use></svg></span>\
 				<div class='c-forum-info t-forum-info' >\
 				<h4 class='c-forum-title t-forum-title'><a href=''> <img src="+response.items[0].volumeInfo.imageLinks.smallThumbnail+"alt='' /></a></h4>\
-				<p class='c-forum-text t-forum-text'>"+response.items[0].volumeInfo.title+"</p>\
-				<p class='c-forum-text t-forum-text'>"+response.items[0].volumeInfo.authors+"</p>\
+				<p class='c-forum-text t-forum-text' id='text1'>TÍTULO\n "+response.items[0].volumeInfo.title+"</p>\
+				<p class='c-forum-text t-forum-text' id='text2'>AUTOR\n "+response.items[0].volumeInfo.authors+"</p>\
 				</div></a>\
-				<div class='o-bar c-forum-footer t-forum-footer'>\
+				<div id= 'footer' class='o-bar c-forum-footer t-forum-footer'>\
 				<ul class='o-bar-left'>\
 				<li class='o-bar-item'>\
 				<span class='o-bar-info'>\
@@ -63,7 +64,7 @@ function buscaimg(livros){
 				</li>\
 				<li class='o-bar-item'>\
 				<span class='o-bar-info'>\
-				<svg class='o-icon o-bar-info-icon'><use xlink:href='#replies'></use></svg>\
+				<svg'class='o-icon o-bar-info-icon'><use xlink:href='#replies'></use></svg>\
 				<span class='o-bar-item-text'>196</span>\
 				</span>\
 				</li>\
@@ -71,7 +72,7 @@ function buscaimg(livros){
 				<ul class='o-bar-right'>\
 				<li class='o-bar-item'>\
 				<a class='o-bar-action' href='' style='color: ' href='' data-toggle='tooltip' data-placement='top' title='Re: ukyuk, by hanakin, 1485294359'>\
-				<svg class='o-icon o-bar-action-icon'><use xlink:href='#last-page'></use></svg>\
+				<svg  class='o-icon o-bar-action-icon'><use xlink:href='#last-page'></use></svg>\
 				</a>\
 				</li>\
 				</ul>\
